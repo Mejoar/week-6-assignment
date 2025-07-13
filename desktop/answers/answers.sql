0590e9b29b77111c380cc0a2e46203796119bdf1
Mejoar <118621175+Mejoar@users.noreply.github.com>
2025-07-13 20:21:53 +0300

    Delete desktop directory

--- desktop/answers/answers.sql
+++
@@ -1,7 +0,0 @@
-
-
----Create a new database called salesDB. The CREATE DATABASE statement is used to create a new database. In this case, the database is named salesDB
-CREATE DATABASE salesDB;
-
---  Drop (delete) the database called demo. The DROP DATABASE statement is used to delete an existing database. Be cautious when using this command, as it will permanently remove the database and all its data.
-DROP DATABASE demo;
